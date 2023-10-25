cd27a094ca109ceac0c6ff869f7d201ca4fdb1d3
BerdinaKatya <kuznkate42@gmail.com>
2023-10-25 16:06:31 +0500

    Initial Commit

---
+++ TipsyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipsyApp
+//
+//  Created by Ekaterina Teliauberdina on 25.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
